d4e9c9303e17d4ca026cc85b51e49c3f20e40816
Daniel King <daniel.oliver.king@gmail.com>
2024-07-04 11:21:18 -0700

    Make the path to the dotfiles repo a parameter.

--- bash_libs/ensure_dotfiles.sh
+++ bash_libs/ensure_dotfiles.sh
@@ -1,13 +1,15 @@
 function ensure_dotfiles {
     if [ "$1" = "-v" ] || [ "$1" = "--verbose" ]; then
         local verbose=true
-        local dotfiles_to_check="${@:2}"
+        local dotfiles_path=$2
+        local dotfiles_to_check="${@:3}"
     elif [[ "$1" =~ ^- ]]; then
         echo "Flag $1 not recognized in call to $FUNCNAME"
         return 1
     else
         local verbose=false
-        local dotfiles_to_check="${@:1}"
+        local dotfiles_path=$1
+        local dotfiles_to_check="${@:2}"
     fi
 
     for dotfile in ${dotfiles_to_check[@]}; do
@@ -22,7 +24,7 @@ function ensure_dotfiles {
             fi
         else
             echo "Dotfile $dotfile does not exist in home directory, creating symlink."
-            ln -s "$HOME/dotfiles/$dotfile" "$HOME/$dotfile"
+            ln -s "$dotfiles_path/$dotfile" "$HOME/$dotfile"
         fi
     done
 }
