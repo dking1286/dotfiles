11f3796c7e8ba74e28aeb8ac53cc6caa9ea6e6c3
Daniel King <daniel.oliver.king@gmail.com>
2025-01-07 20:28:14 -0800

    Delete the cloned emacs repo after installation.

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -10,8 +10,10 @@ set -e
 ./install_scripts/ubuntu/install_gcloud_cli.sh
 ./install_scripts/ubuntu/install_gnome_boxes.sh
 ./install_scripts/ubuntu/install_virtualbox.sh
-./install_scripts/ubuntu/install_emacs.sh
 ./install_scripts/ubuntu/install_vs_code.sh
+./install_scripts/ubuntu/install_emacs.sh
+# Installation of emacs clones the emacs repo from Github, delete it.
+rm -rf emacs
 
 sudo apt update
 sudo apt -y upgrade
