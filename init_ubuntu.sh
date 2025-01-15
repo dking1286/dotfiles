00f392666eda233baae637c6339510150c28e457
Daniel O King <daniel.oliver.king@gmail.com>
2025-01-15 12:30:05 -0800

    Rearrange init_ubuntu.sh to be more readable.

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -4,18 +4,23 @@ set -e
 
 sudo apt update
 
+# Development tools
 ./install_scripts/ubuntu/install_git.sh
+./install_scripts/ubuntu/install_gcloud_cli.sh
+
+# Programming languages
 ./install_scripts/ubuntu/install_java.sh
 ./install_scripts/ubuntu/install_clojure.sh
 ./install_scripts/ubuntu/install_node.sh
+
+# Editors
 ./install_scripts/ubuntu/install_jetbrains_toolbox.sh
-./install_scripts/ubuntu/install_gcloud_cli.sh
-./install_scripts/ubuntu/install_gnome_boxes.sh
-./install_scripts/ubuntu/install_virtualbox.sh
 ./install_scripts/ubuntu/install_vs_code.sh
 ./install_scripts/ubuntu/install_emacs.sh
-# Installation of emacs clones the emacs repo from Github, delete it.
-rm -rf emacs
+
+# Virtualization tools
+./install_scripts/ubuntu/install_gnome_boxes.sh
+./install_scripts/ubuntu/install_virtualbox.sh
 
 sudo apt update
 sudo apt -y upgrade
